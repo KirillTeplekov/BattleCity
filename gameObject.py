f8591697ab697a0b98335efbd0b26f4feccb51db
Kirill <Kirill.Teplekov@yandex.ru>
2019-02-13 23:05:01 +0300

    Fix code according to main.py

--- gameObject.py
+++ gameObject.py
@@ -1,5 +1,5 @@
 import pygame
-from main import load_image, game_over, player
+from main import load_image, player
 from random import choice, randint
 
 # Game's group
@@ -87,9 +87,8 @@ enemy_images = {'btr': {'btr_u': load_image(path_to_enemy + 'btr/up.png'),
                     'bonus_r': load_image(
                         path_to_enemy + 'standard/bonus_right.png')}}
 
+
 # Game's board border
-
-
 class Border(pygame.sprite.Sprite):
     def __init__(self, x, y, w, h, dir):
         super().__init__(collide_group)
@@ -130,20 +129,20 @@ class Water(Tile):
         else:
             self.index = 0
 
+
 # Castle's flag
-
-
 class Flag(Tile):
     flag_on = load_image('other/flag_on.png')
     flag_off = load_image('other/flag_off.png')
 
     def __init__(self, posx, posy):
         super().__init__(Flag.flag_on, posx, posy, collide_group)
+        self.state = 'on'
 
     def update(self):
         if pygame.sprite.spritecollideany(self, bullet_group):
             self.image = Flag.flag_off
-            game_over()
+            self.state = 'off'
 
 
 class Brick(Tile):
@@ -170,14 +169,19 @@ class Brick(Tile):
 
 
 class Bonus(Tile):
-    bonus_images = {'helmet': load_image('other/bonus/helmet.png'), 'clock': load_image('other/bonus/clock.png'),
-                    'shovel': load_image('other/bonus/shovel.png'), 'tank': load_image('other/bonus/tank.png'),
-                    'star': load_image('other/bonus/star.png'), 'grenade': load_image('other/bonus/grenade.png')}
+    bonus_images = {'helmet': load_image('other/bonus/helmet.png'),
+                    'clock': load_image('other/bonus/clock.png'),
+                    'shovel': load_image('other/bonus/shovel.png'),
+                    'tank': load_image('other/bonus/tank.png'),
+                    'star': load_image('other/bonus/star.png'),
+                    'grenade': load_image('other/bonus/grenade.png')}
 
     def __init__(self, posx, posy):
         bonus = ['helmet', 'clock', 'shovel', 'tank', 'star', 'grenade']
         self.bonus_type = choice(bonus)
-        super().__init__(Bonus.bonus_images[self.bonus_type], (posx - x_indent) // tile_width, (posy - y_indent) // tile_height,
+        super().__init__(Bonus.bonus_images[self.bonus_type],
+                         (posx - x_indent) // tile_width,
+                         (posy - y_indent) // tile_height,
                          bonus_group)
 
     def update(self, player):
@@ -185,10 +189,6 @@ class Bonus(Tile):
             if self.bonus_type == 'helmet':
                 player.shield = True
                 Shield(player)
-            if self.bonus_type == 'clock':
-                pass
-            if self.bonus_type == 'shovel':
-                pass
             if self.bonus_type == 'tank':
                 player.hp += 1
             if self.bonus_type == 'star':
@@ -198,9 +198,8 @@ class Bonus(Tile):
                     enemy.kill()
             self.kill()
 
+
 # Tanks
-
-
 class Tanks(pygame.sprite.Sprite):
     def __init__(self, image, posx, posy):
         super().__init__(all_sprites)
@@ -285,7 +284,8 @@ class Player(Tanks):
             self.rect.topleft = (new_pos)
 
     def rotate(self):
-        self.image = player_images['lvl' + str(self.lvl) + '_' + self.direction]
+        self.image = player_images[
+            'lvl' + str(self.lvl) + '_' + self.direction]
 
 
 class Enemy(Tanks):
@@ -327,10 +327,12 @@ class Enemy(Tanks):
 
         if self.bonus:
             if self.flash < 3:
-                self.image = enemy_images[self.type + '_bonus']['bonus_' + self.direction]
+                self.image = enemy_images[self.type + '_bonus'][
+                    'bonus_' + self.direction]
                 self.flash += 1
             elif self.flash < 6:
-                self.image = enemy_images[self.type][self.type + '_' + self.direction]
+                self.image = enemy_images[self.type][
+                    self.type + '_' + self.direction]
                 self.flash += 1
             else:
                 self.flash = 0
@@ -431,7 +433,7 @@ class Bullet(pygame.sprite.Sprite):
 class Boom(pygame.sprite.Sprite):
     boom_images = [load_image('other/boom_1.png'),
                    load_image('other/boom_2.png'),
-                   load('other/boom_3.png')]
+                   load_image('other/boom_3.png')]
 
     def __init__(self, posx, posy, direction):
         super().__init__(boom_group)
@@ -456,10 +458,10 @@ class Boom(pygame.sprite.Sprite):
     def update(self):
         if self.boom == 0:
             self.boom = 1
-            self.image = Bullet.boom_images[self.boom]
+            self.image = Boom.boom_images[self.boom]
         elif self.boom == 1:
             self.boom = 2
-            self.image = Bullet.boom_images[self.boom]
+            self.image = Boom.boom_images[self.boom]
             self.kill()
 
 
@@ -468,11 +470,11 @@ class Shield(pygame.sprite.Sprite):
     shield_2 = load_image('other/shield_2.png')
 
     def __init__(self, owner):
-        print('ok')
         super().__init__(shield_group)
         self.owner = owner
         self.image = Shield.shield_1
-        self.rect = self.image.get_rect().move(self.owner.rect.x, self.owner.rect.y)
+        self.rect = self.image.get_rect().move(self.owner.rect.x,
+                                               self.owner.rect.y)
 
     def update(self):
         self.rect.topleft = self.owner.rect.topleft
